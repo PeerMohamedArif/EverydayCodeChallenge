23d9b953a1a785c51c0e5232e9d5c1a9ae633676
PeerMohamedArif <arifoct24@gmail.com>
2024-10-27 14:35:03 +0530

    Create main.py

---
+++ day25/USstatesGame/main.py
@@ -0,0 +1,41 @@
+import turtle
+import pandas as pd
+screen=turtle.Screen()
+screen.title("US States Guess Game")
+image="blank_states_img.gif"
+
+screen.addshape(image)
+turtle.shape(image)
+guessed_states=[]
+# def get_mouse_click_coordinates(x, y):
+#     print(x, y)
+# turtle.onscreenclick(get_mouse_click_coordinates)
+# turtle.mainloop()# to keep the window open for multiple clicks
+while len(guessed_states)<50:
+
+    answer_state=screen.textinput(title=f" {len(guessed_states)}/50 States correct ",
+                                  prompt="What is another state name?").title()
+    data=pd.read_csv("50_states.csv")
+    all_states=data.state.to_list()
+
+    if answer_state == 'exit':
+        missing_states=[]
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data=pd.DataFrame(missing_states)
+        new_data.to_csv("States_to_learn.csv")
+        break
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t=turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        coordinates=data[data["state"]==answer_state]
+        t.goto(int(coordinates["x"].item()),int(coordinates["y"].item())) # item is used to gets the first element alone
+        t.write(answer_state)
+
+screen.exitonclick()
+
+
