266cfde29291f7057f7c8eec72c0567feb8ec455
PeerMohamedArif <arifoct24@gmail.com>
2024-10-31 10:06:32 +0530

    Update NATOalphabetProject.py

--- day26/NATOalphabetProject.py
+++ day26/NATOalphabetProject.py
@@ -1,1 +1,7 @@
+import pandas as pd
+data=pd.read_csv("nato_phonetic_alphabet.csv")
+data_dict={row.letter :row.code for index,row in data.iterrows()}
 
+word=input("Enter a word: ").upper()
+output=[data_dict[letter] for letter in word]
+print(output)
