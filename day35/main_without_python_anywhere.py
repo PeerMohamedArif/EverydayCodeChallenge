f99cececf6b4abee6abb87bd74230febfae094e8
PeerMohamedArif <arifoct24@gmail.com>
2024-11-21 12:51:46 +0530

    Create main_without_python_anywhere.py

---
+++ day35/main_without_python_anywhere.py
@@ -0,0 +1,40 @@
+import requests
+import os
+from twilio.rest import Client
+
+
+account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+
+
+OWM_endpoint="" # url
+api_key= "" # api key
+weather_params={
+    "lat":-3.386925,
+    "lon":36.682995,
+    "appid":api_key,
+    "cnt": 4
+
+}
+
+
+
+will_rain= False
+response=requests.get(url=OWM_endpoint,params=weather_params)
+response.raise_for_status()
+weather_data=response.json()
+#print(weather_data["list"][0]["weather"][0]["id"])
+for hour_data in weather_data["list"]:
+    condition_code=(hour_data["weather"][0]["id"])
+    if int(condition_code)<700:
+        will_rain=True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="Rain Alert,Its going to rain today ,Be sure to take an umbrella",
+        from_="", # your trail number
+        to="", # number with which we logged in
+    )
+
+    print(message.status)
