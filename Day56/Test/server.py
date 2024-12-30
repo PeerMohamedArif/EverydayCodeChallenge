23859857a048daca83a9a62fc406360d3d503ada
PeerMohamedArif <arifoct24@gmail.com>
2024-12-30 10:51:44 +0530

    Update server.py

--- Day56/Test/server.py
+++ Day56/Test/server.py
@@ -7,10 +7,15 @@ app = Flask(__name__)
 #     # our HTML should be in the templates folder for this to work
 #     return render_template("index.html")
 
-@app.route('/')
-def angela_cv():
-    # static files such as CSS, Javascript and images have to be kept in the static folder
-    return render_template("Angela.html")
+# @app.route('/')
+# def home():
+#     # our HTML should be in the templates folder for this to work
+#     return render_template("index1.html")
+
+# @app.route('/')
+# def angela_cv():
+#     # static files such as CSS, Javascript and images have to be kept in the static folder
+#     return render_template("Angela.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
