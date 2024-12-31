31e18c76ffcbfa69ed98ce0d199f3f3346642777
PeerMohamedArif <arifoct24@gmail.com>
2024-12-31 09:43:03 +0530

    Create server.py

---
+++ Day56/TemplateWebsite/server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    # our HTML should be in the templates folder for this to work
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
